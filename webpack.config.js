01cff05842539296d3fa13614a7dca5c350e4a80
Muhammad Sami <muhammadsami290@outlook.com>
2020-06-16 17:41:06 +0500

    Development Tools setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const htmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new htmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /\node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
