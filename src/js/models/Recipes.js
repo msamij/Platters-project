b0e46e54f53bca15094e3dbc6e0a67b97b3004c7
Muhammad Sami <muhammadsami290@outlook.com>
2021-12-17 12:03:10 +0500

    Update Recipes.js

--- src/js/models/Recipes.js
+++ src/js/models/Recipes.js
@@ -8,7 +8,7 @@ export default class Recipe {
   async getRecipes() {
     try {
       const response = await axios(
-        `https://cors-anywhere.herokuapp.com/https://api.spoonacular.com/recipes/${this.id}/information?apiKey=${apiKey}`
+        `https://api.spoonacular.com/recipes/${this.id}/information?apiKey=${apiKey}`
       );
       this.title = response.data.title;
       this.ingredients = response.data.extendedIngredients;
